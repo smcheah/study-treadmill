255f0dad49a91fd4900d6be2a279985fcc8de8ba
Simone Cheah <simone_cheah@hotmail.com>
2021-01-25 20:22:49 +1100

    add login styling

--- client/src/pages/login/index.js
+++ client/src/pages/login/index.js
@@ -1,9 +1,17 @@
 import React from "react";
 
 const LoginPage = () => {
-    return <div>
-        login
-    </div>;
+    return <>
+        <div className="flex flex-col min-h-screen items-center justify-center">
+            <input className="text-5xl md:text-8xl text-center focus:outline-none text-white bg-gray-400 placeholder-white max-w-6xl w-9/12 m-3" type="text" placeholder="USERNAME" />
+            <input className="text-5xl md:text-8xl text-center focus:outline-none text-white bg-gray-400 placeholder-white max-w-6xl w-9/12 m-3" type="password" placeholder="PASSWORD" />
+            <div className="mt-6">
+                <i className="fas fa-level-up-alt transform rotate-90 text-2xl md:text-4xl p-4"></i>
+                <button className=" text-2xl md:text-4xl font-semibold m-1 p-3 hover:bg-yellow-300 hover:border-transparent border-gray-400 border-solid border-4 rounded-md" type="submit" id="login-submit">SUBMIT</button>
+            </div>
+
+        </div>
+    </>;
 };
 
 export default LoginPage;
