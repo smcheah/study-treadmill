310ebfd6d866d80cca3b3e3b8079b8174352285a
Simone Cheah <simone_cheah@hotmail.com>
2021-01-29 01:20:28 +1100

    add ondelete for user model

--- models/user.js
+++ models/user.js
@@ -3,18 +3,24 @@ const bcrypt = require("bcryptjs");
 // Creating our User model
 module.exports = function (sequelize, DataTypes) {
     const User = sequelize.define("User", {
-        // The email cannot be null, and must be a proper email before creation
         username: {
             type: DataTypes.STRING,
             allowNull: false,
             unique: true
         },
-        // The password cannot be null
         password: {
             type: DataTypes.STRING,
             allowNull: false
         }
     });
+    User.associate = function (models) {
+        // Associating Author with Posts
+        // When an Author is deleted, also delete any associated Posts
+        User.hasMany(models.Task, {
+            onDelete: "cascade"
+        });
+    };
+
     // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
     User.prototype.validPassword = function (password) {
         return bcrypt.compareSync(password, this.password);
