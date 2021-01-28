8fbd55cb853e2af0c32d63483c911d7ef51f3591
Simone Cheah <simone_cheah@hotmail.com>
2021-01-29 01:26:54 +1100

    fix syntax

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -20,10 +20,10 @@ const HomePage = () => {
                             <button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-expanded="false">
                                 <span className="sr-only">Open main menu</span>
                                 <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
                                 </svg>
                                 <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
                                 </svg>
                             </button>
                         </div>
@@ -55,7 +55,7 @@ const HomePage = () => {
                 <span className="flex justify-center pt-10">
                     <i className="fas fa-level-up-alt transform rotate-90 text-5xl p-4"></i>
                     <h2 className="md:text-6xl text-3xl p-4">
-                        <Link to="/login" className="hover:text-yellow-400 underline">Login</Link> / <Link to="/login" className="hover:text-yellow-500 underline">Sign Up</Link>
+                        <Link to="/login" className="hover:text-yellow-400 underline">Login</Link> / <Link to="/signup" className="hover:text-yellow-500 underline">Sign Up</Link>
                     </h2>
                 </span>
 
