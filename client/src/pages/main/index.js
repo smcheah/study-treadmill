db490ee65250288c3101b34d5f751f50a45550b6
Simone Cheah <simone_cheah@hotmail.com>
2021-02-02 20:48:49 +1100

    fix functionality and abstract guy

--- client/src/pages/main/index.js
+++ client/src/pages/main/index.js
@@ -1,125 +1,134 @@
-import React from "react";
-import { motion } from "framer-motion";
-import { useRouteMatch, Link } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import Guy from "../../components/guy";
+import Timer from "../../components/timer";
+import Modal from "../../components/modal";
+import UserContext from "../../util/user-context";
+import testTimelineData from "../../util/timeline-data";
+import { Chrono } from "react-chrono";
+import API from "../../util/API";
+import "./style.css";
+
+/**
+ *
+ * see the username in header
+ *
+ * add functionality to creating and deleting a new task
+ * and reward
+ *
+ * add the tasks to teh user
+ *
+ * create timeline functionality
+ * auto scroll as time goes on
+ * fade past tasks
+ *
+ */
 
 const MainPage = () => {
-    const bodyVariant = {
-        initial: {},
-        animate: {
-            y: 5,
-            transition: {
-                repeat: Infinity,
-                repeatType: "reverse",
-                duration: 2
-            }
-        },
+
+    const [modal, setModal] = useState(false);
+    const [user, setUser] = useState("User");
+    const [userId, setUserId] = useState("");
+    const [timelineData, setTimelineData] = useState([]);
+
+    const showModal = (e) => {
+        e.preventDefault();
+        setModal(true);
     };
-    const headNodVariant = {
-        initial: {},
-        animate: {
-            x: 3,
-            y: 3,
-            rotate: 7,
-            transition: {
-                repeat: Infinity,
-                repeatType: "reverse",
-                duration: 2
-            }
-        },
-    };
-    const leg2ClimbVariant = {
-        initial: {},
-        animate: {
-            x: 67,
-            y: -10,
-            rotate: -50,
-            transition: {
-                repeat: Infinity,
-                repeatType: "reverse",
-                duration: 4
-            }
-        },
-    };
-    const leg1ClimbVariant = {
-        initial: {},
-        animate: {
-            x: -59,
-            y: 25,
-            rotate: 60,
-            transition: {
-                repeat: Infinity,
-                repeatType: "reverse",
-                duration: 4
-            }
-        },
+    const closeModal = (e) => {
+        e.preventDefault();
+        setModal(false);
     };
 
+    const getUsername = () => {
+        API.getUser().then(data => {
+            // console.log(data);
+            setUserId(data.data[0].id);
+            setUser(data.data[0].username);
+        });
+    };
+    getUsername();
 
-    return <div className="main grid grid-cols-3 grid-rows-timeline">
-        <div className="task-info">
-            <div className="current-task">
-                current task: hello
-            </div>
-            <div className="upcoming-reward">
-                reward: world
-            </div>
+    const getTimeLineData = () => {
+        API.getTasks().then(data => {
+            console.log(data.data);
+            // if (data.data) {
+            //     setTimelineData(data.data);
+            // }
+        });
+    };
+    getTimeLineData();
+
+    // const { user } = useContext(UserContext);
+    return <div className="main grid grid-cols-main grid-rows-main min-h-full w-full">
+
+        <div className="z-40 fixed">
+            <Modal
+                isShown={ modal }
+                userId={ userId }
+                closeModal={ closeModal } />
         </div>
 
-        <div className="running-man inset-x-1/3 inset-y-1/4 border-solid border-2 border-black rounded-full block">
-            <div className="countdown">
-                5:28 s
+        <section className="border-solid border-black  col-start-1 col-end-4 self-center">
+            <h1 className="text-6xl text-center">{ user }'s Study Session</h1>
+        </section>
+
+        <section className="flex flex-col  border-solid border-black ">
+            <button id="add-task"
+                className="relative max-w-max top-16 left-0 btn-main bg-green-400 hover:bg-green-500 focus:ring-green-400"
+                onClick={ showModal }>
+                + add task
+                </button>
+            <div
+                className="left-0 m-auto max-w-sm border-solid border-black border p-4">
+                <h2>Task</h2>
+                <span>time</span>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                </p> 
             </div>
+        </section>
 
-            <div className="messages">
-                Hey good lookin'!
+        <section className="border-solid border-black ">
+            <div className="min-h-full m-4 relative  border-solid border-black border-0  block">
+                <div className="absolute top-3 max-h-96 bg-yellow-200 rounded-full">
+                    <div className="timer text-center text-4xl m-10">
+                        <Timer />
+                    </div>
+                    <div className="z-10"><Guy /></div>
+                    <div id="guy-bg" className="m-auto absolute -top-56 z-0"></div>
+                </div>
+
             </div>
+        </section>
 
-            <svg className="relative border-solid border-2 border-black " width="281" height="411" viewBox="0 0 281 411" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <motion.g id="guy"
-                    variants={ bodyVariant } initial="initial" animate="animate">
-                    <motion.g id="guy-head"
-                        variants={ headNodVariant } initial="initial" animate="animate">
-                        <circle id="head" cx="195" cy="64" r="44" fill="#C4C4C4" />
-                    </motion.g>
-                    <motion.g id="guy-body">
-                        <rect id="body" x="166.519" y="84.933" width="36.3451" height="152" rx="18.1726" transform="rotate(16.8859 166.519 84.933)" fill="#C4C4C4" />
-                    </motion.g>
-                    <motion.g id="guy-arm-1">
-                        <rect id="Rectangle 2" x="164" y="100.998" width="30" height="125" rx="15" transform="rotate(-11.5336 164 100.998)" fill="#C4C4C4" />
-                        <rect id="Rectangle 7" x="239.862" y="210.421" width="30" height="50" rx="15" transform="rotate(117.22 239.862 210.421)" fill="#C4C4C4" />
-                    </motion.g>
-                    <motion.g id="guy-arm-2">
-                        <rect id="Rectangle 8" x="172.149" y="90" width="30" height="125" rx="15" transform="rotate(47.0473 172.149 90)" fill="#C4C4C4" />
-                        <rect id="Rectangle 9" x="117.425" y="212.719" width="30" height="50" rx="15" transform="rotate(175.801 117.425 212.719)" fill="#C4C4C4" />
-                    </motion.g>
-                    <motion.g id="guy-leg-1"
-                        variants={ leg1ClimbVariant } initial="initial" animate="animate">
-                        <rect id="Rectangle 8_2" x="203.764" y="255.793" width="35" height="99" rx="17.5" transform="rotate(-18.7487 203.764 255.793)" fill="#C4C4C4" />
-                        <rect id="Rectangle 7_2" x="120.361" y="230.567" width="35" height="125.298" rx="17.5" transform="rotate(-62.6266 120.361 230.567)" fill="#C4C4C4" />
-                    </motion.g>
-                    <motion.g id="guy-leg-2"
-                        variants={ leg2ClimbVariant } initial="initial" animate="animate">
-                        <rect id="Rectangle 9_2" x="146.091" y="288.106" width="35" height="99" rx="17.5" transform="rotate(40.7638 146.091 288.106)" fill="#C4C4C4" />
-                        <rect id="Rectangle 10" x="125.514" y="203.436" width="35" height="125.298" rx="17.5" transform="rotate(-3.11419 125.514 203.436)" fill="#C4C4C4" />
-                    </motion.g>
-                    <motion.g id="guy-bandana"
-                        variants={ headNodVariant } initial="initial" animate="animate">
-                        <rect id="Rectangle 11" x="145.667" y="51.189" width="97.1828" height="13" rx="6.5" transform="rotate(-3.95622 145.667 51.189)" fill="#FF8080" />
-                        <rect id="Rectangle 12" x="118.49" y="40" width="32" height="8" rx="4" transform="rotate(18.1381 118.49 40)" fill="#FF8080" />
-                        <rect id="Rectangle 13" x="116" y="56" width="32" height="8" rx="4" fill="#FF8080" />
-                    </motion.g>
-                </motion.g>
-            </svg>
-        </div>
+        <section className="flex flex-col relative border-solid border-black ">
+            {/* <button id="friend-alert"
+                className="absolute right-0 btn-main bg-yellow-400 hover:bg-yellow-500 focus:ring-yellow-400">
+                friend alert
+                </button> */}
+            <Link to="/main/stats"
+                className="absolute top-16 right-0">
+                <button
+                    className="btn-main bg-blue-400 hover:bg-blue-500 focus:ring-blue-400">
+                    Statistics Overview
+                    </button>
+            </Link>
+            <div
+                className="m-auto max-w-sm current-task border-solid border-black border p-4">
+                <h2>Reward</h2>
+                <span>time</span>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                </p>
+            </div>
+        </section>
 
-        <div className="stats-page">
-            <Link to="/main/stats"><button>stats</button></Link>
-        </div>
-
-        <div className="timeline row-start-2">
-            timeline
-        </div>
-
+        <section id="timeline" className="col-start-1 col-end-4 row-start-3 border-solid border-black ">
+            <div style={ { width: "100%", height: "100%" } }>
+                <Chrono items={ testTimelineData } mode="HORIZONTAL" />
+            </div>
+        </section>
     </div>;
 };
 
