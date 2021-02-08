deec40cfd7c458529c5b7ee877232fdce40d7775
Simone Cheah <simone_cheah@hotmail.com>
2021-02-08 13:05:17 +1100

    fix queries

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -3,29 +3,19 @@ const db = require("../models");
 const router = require("express").Router();
 const passport = require("../config/passport");
 
-router.get("/signup", function (req, res) {
-    db.User.findAll().then(data => {
-        res.json(data);
-    });
-});
-router.get("/login", function (req, res) {
-    db.User.findAll().then(data => {
-        res.json(data);
-    });
-});
-router.get("/user", function (req, res) {
-    db.User.findAll().then(data => {
-        res.json(data);
-    });
-});
-router.get("/tasks", function (req, res) {
+// task db
+router.get("/tasks/:userId", function (req, res) {
     db.Task.findAll().then(data => {
         res.json(data);
     });
 });
-
+router.delete("/tasks/:userId", function (req, res) {
+    db.Task.findAll().then(data => {
+        res.json(data);
+    });
+});
 router.post("/tasks", async (req, res) => {
-    console.log("hi");
+    console.log(req);
     const { title, body, reward, date, time } = req.body;
     try {
         console.log("post task in api route:" + req.body);
@@ -40,6 +30,32 @@ router.post("/tasks", async (req, res) => {
         res.status(401).json(err);
     }
 });
+
+// user db
+router.get("/signup", function (req, res) {
+    db.User.findAll().then(data => {
+        res.json(data);
+    });
+});
+router.get("/login", function (req, res) {
+    db.User.findAll().then(data => {
+        res.json(data);
+    });
+});
+router.get("/user/:id", function (req, res) {
+    db.User.findOne({
+        where: { id: req.params.id }
+    }).then(({ id, username }) => {
+        res.json({ id, username });
+    });
+});
+router.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+});
+router.delete("/user/:id", (req, res) => {
+
+})
 router.post("/login", passport.authenticate("local"), (req, res) => {
     res.json(req.user);
 });
@@ -56,86 +72,5 @@ router.post("/signup", async (req, res) => {
         res.status(401).json(err);
     }
 });
-router.get("/logout", (req, res) => {
-    req.logout();
-    res.redirect("/");
-});
 
 module.exports = router;
-
-// module.exports = 
-// function hi (app) {
-//     // Using the passport.authenticate middleware with our local strategy.
-//     // If the user has valid login credentials, send them to the members page.
-//     // Otherwise the user will be sent an error
-//     app.post("/api/login", passport.authenticate("local"), (req, res) => {
-//         // Sending back a response to server in json
-//         // and authenticates it
-//         res.json(req.user);
-//     });
-
-//     // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
-//     // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
-//     // otherwise send back an error
-//     // app.post("/api/signup", (req, res) => {
-//     //     db.User.create({
-//     //         username: req.body.username,
-//     //         password: req.body.password
-//     //     }).then(() => {
-//     //         res.redirect(307, "/api/login");
-//     //     }).catch(err => {
-//     //         res.status(401).json(err);
-//     //     });
-//     // });
-//     app.post("/api/signup", (req, res) => {
-//         db.User.create({
-//             username: req.body.username,
-//             password: req.body.password
-//         }).then((data) => {
-//             res.json(data);
-//         }).catch(err => {
-//             res.status(401).json(err);
-//         });
-//     });
-
-//     app.get("/api/signup", function (req, res) {
-//         db.User.findAll().then(data => {
-//             res.json(data);
-//         });
-//     });
-//     app.get("/api/login", function (req, res) {
-//         db.User.findAll().then(data => {
-//             res.json(data);
-//         });
-//     });
-
-//     // Route for logging user out
-//     app.get("/logout", (req, res) => {
-//         req.logout();
-//         res.redirect("/");
-//     });
-
-//     // get user
-//     app.get("/api/user/:id", function (req, res) {
-//         db.User.findOne({
-//             where: {
-//                 id: req.params.id
-//             }
-//         }).then(function (data) {
-//             res.json(data);
-//         });
-//     });
-
-//     // Route for getting some data about our user to be used client side
-//     app.get("/api/user_data", (req, res) => {
-//         if (!req.user) {
-//             // The user is not logged in, send back an empty object
-//             res.json({});
-//         } else {
-//             res.json({
-//                 username: req.user.username,
-//                 id: req.user.id
-//             });
-//         }
-//     });
-// };
